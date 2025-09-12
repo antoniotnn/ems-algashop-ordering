163733c2fcf045674e9fc76ada0b315b058f42fc
Antonio Pires <tonan@outlook.com.br>
2025-09-12 06:41:06 -0300

    Refactor Customer Entity toward a rich model

--- src/test/java/com/algaworks/algashop/ordering/CustomerTest.java
+++ src/test/java/com/algaworks/algashop/ordering/CustomerTest.java
@@ -3,16 +3,25 @@ package com.algaworks.algashop.ordering;
 import com.algaworks.algashop.ordering.domain.entity.Customer;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 class CustomerTest {
 
     @Test
     void testingCustomer() {
-        Customer customer = new Customer();
-        customer.setId(null);
-        customer.setFullName("Alex Silva");
-        customer.setDocument(null);
-        customer.setLoyaltyPoints(10);
+        Customer customer = new Customer(
+                UUID.randomUUID(),
+                "Jhon Doe",
+                LocalDate.of(1991, 7, 5),
+                "jhon.doe@email.com",
+                "478-256-2504",
+                "255-08-0578",
+                true,
+                OffsetDateTime.now()
+        );
+
+        customer.addLoyaltyPoints(10);
     }
 }
