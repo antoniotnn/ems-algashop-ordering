a26c830213d43e5f588a137ae2848165fe551ec7
Antonio Pires <tonan@outlook.com.br>
2025-11-07 06:42:33 -0300

    Queries for SUM and COUNTS using filters

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepository.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,5 +20,29 @@ public interface OrderPersistenceEntityRepository extends JpaRepository<OrderPer
     List<OrderPersistenceEntity> placedByCustomerInYear(
             @Param("customerId") UUID customerId,
             @Param("year") Integer year
+    );
+
+    @Query("""
+            SELECT COUNT(o)
+            FROM OrderPersistenceEntity o
+            WHERE o.customer.id = :customerId
+            AND YEAR(o.placedAt) = :year
+            AND o.paidAt is NOT NULL
+            AND o.canceledAt is NULL
+    """)
+    long salesQuantityByCustomerInYear(
+            @Param("customerId") UUID customerId,
+            @Param("year") Integer year
+    );
+
+    @Query("""
+            SELECT COALESCE(SUM(o.totalAmount), 0)
+            FROM OrderPersistenceEntity o
+            WHERE o.customer.id = :customerId
+            AND o.canceledAt is NULL
+            AND o.paidAt is NOT NULL
+    """)
+    BigDecimal totalSoldForCustomer(
+            @Param("customerId") UUID customerId
     );
 }
