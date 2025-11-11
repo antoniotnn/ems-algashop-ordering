2f1e77988891adc009f9a815f7795655592c2fe1
Antonio Pires <tonan@outlook.com.br>
2025-11-11 06:40:26 -0300

    Developing a Domain Service - CustomerLoyaltyPointsService

--- src/main/java/com/algaworks/algashop/ordering/domain/model/exception/DomainException.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/exception/DomainException.java
@@ -1,6 +1,14 @@
 package com.algaworks.algashop.ordering.domain.model.exception;
 
 public class DomainException extends RuntimeException {
+
+    public DomainException() {
+    }
+
+    public DomainException(Throwable cause) {
+        super(cause);
+    }
+
     public DomainException(String message, Throwable cause) {
         super(message, cause);
     }
