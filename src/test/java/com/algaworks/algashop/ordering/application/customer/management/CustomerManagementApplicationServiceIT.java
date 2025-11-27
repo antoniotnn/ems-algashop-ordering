941a1496a08f2da71217637d680eed5b14a129dc
Antonio Pires <tonan@outlook.com.br>
2025-11-27 06:16:30 -0300

    Customizing property mapping

--- src/test/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationServiceIT.java
@@ -45,6 +45,7 @@ class CustomerManagementApplicationServiceIT {
         Assertions.assertThat(customerOutput.getFirstName()).isEqualTo("John");
         Assertions.assertThat(customerOutput.getLastName()).isEqualTo("Doe");
         Assertions.assertThat(customerOutput.getEmail()).isEqualTo("johndoe@email.com");
+        Assertions.assertThat(customerOutput.getBirthDate()).isEqualTo(LocalDate.of(1991, 7, 5));
         Assertions.assertThat(customerOutput.getRegisteredAt()).isNotNull();
     }
 
