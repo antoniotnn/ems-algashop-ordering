5bdc780fa995fb2da5f35f719269a5c1e4cc9998
Antonio Pires <tonan@outlook.com.br>
2026-05-11 08:42:19 -0300

    Organizing packages

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssemblerTest.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssemblerTest.java
@@ -1,9 +1,9 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.assembler;
 
 
-import com.algaworks.algashop.ordering.domain.model.order.Order;
-import com.algaworks.algashop.ordering.domain.model.order.OrderItem;
-import com.algaworks.algashop.ordering.domain.model.order.OrderTestDataBuilder;
+import com.algaworks.algashop.ordering.core.domain.model.order.Order;
+import com.algaworks.algashop.ordering.core.domain.model.order.OrderItem;
+import com.algaworks.algashop.ordering.core.domain.model.order.OrderTestDataBuilder;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.CustomerPersistenceEntityTestDataBuilder;
 import com.algaworks.algashop.ordering.infrastructure.persistence.customer.CustomerPersistenceEntityRepository;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntityTestDataBuilder;
