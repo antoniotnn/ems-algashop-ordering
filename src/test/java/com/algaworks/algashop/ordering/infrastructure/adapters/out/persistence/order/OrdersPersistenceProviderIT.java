c3da200179942efdfa38741d8770fdc278e970d1
Antonio Pires <tonan@outlook.com.br>
2026-05-12 09:58:23 -0300

    Refactoring Tests to Follow the Hexagonal Architecture

---
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/persistence/order/OrdersPersistenceProviderIT.java
@@ -0,0 +1,81 @@
+package com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.order;
+
+import com.algaworks.algashop.ordering.core.domain.model.order.Order;
+import com.algaworks.algashop.ordering.core.domain.model.order.OrderStatus;
+import com.algaworks.algashop.ordering.core.domain.model.order.OrderTestDataBuilder;
+import com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.AbstractPersistenceIT;
+import com.algaworks.algashop.ordering.infrastructure.config.auditing.SpringDataAuditingConfig;
+import com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.customer.CustomerPersistenceEntityAssembler;
+import com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.customer.CustomerPersistenceEntityDisassembler;
+import com.algaworks.algashop.ordering.infrastructure.adapters.out.persistence.customer.CustomersPersistenceProvider;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Import({
+        OrdersPersistenceProvider.class,
+        OrderPersistenceEntityAssembler.class,
+        OrderPersistenceEntityDisassembler.class,
+        CustomersPersistenceProvider.class,
+        CustomerPersistenceEntityAssembler.class,
+        CustomerPersistenceEntityDisassembler.class,
+        SpringDataAuditingConfig.class
+})
+@TestPropertySource(properties = "spring.flyway.locations=classpath:db/migration,classpath:db/testdata")
+class OrdersPersistenceProviderIT extends AbstractPersistenceIT {
+
+    private final OrdersPersistenceProvider persistenceProvider;
+    private final OrderPersistenceEntityRepository entityRepository;
+
+    @Autowired
+    public OrdersPersistenceProviderIT(OrdersPersistenceProvider persistenceProvider,
+                                       OrderPersistenceEntityRepository entityRepository) {
+        this.persistenceProvider = persistenceProvider;
+        this.entityRepository = entityRepository;
+    }
+
+    @Test
+    void shouldUpdateAndKeepPersistenceEntityState() {
+        Order order = OrderTestDataBuilder.anOrder().status(OrderStatus.PLACED).build();
+        long orderId = order.id().value().toLong();
+        persistenceProvider.add(order);
+
+        var persistenceEntity = entityRepository.findById(orderId).orElseThrow();
+
+        Assertions.assertThat(persistenceEntity.getStatus()).isEqualTo(OrderStatus.PLACED.name());
+
+        Assertions.assertThat(persistenceEntity.getCreatedByUserId()).isNotNull();
+        Assertions.assertThat(persistenceEntity.getLastModifiedAt()).isNotNull();
+        Assertions.assertThat(persistenceEntity.getLastModifiedByUserId()).isNotNull();
+
+        order = persistenceProvider.ofId(order.id()).orElseThrow();
+        order.markAsPaid();
+        persistenceProvider.add(order);
+
+        persistenceEntity = entityRepository.findById(orderId).orElseThrow();
+
+        Assertions.assertThat(persistenceEntity.getStatus()).isEqualTo(OrderStatus.PAID.name());
+
+        Assertions.assertThat(persistenceEntity.getCreatedByUserId()).isNotNull();
+        Assertions.assertThat(persistenceEntity.getLastModifiedAt()).isNotNull();
+        Assertions.assertThat(persistenceEntity.getLastModifiedByUserId()).isNotNull();
+    }
+
+    // resolvendo LazyInitializationException anotando a classe com Transactional true,
+    // e somente métodos que tem escrita com Transactional false (é padrão, mas a sugestão é deixar explícito)
+    // ver classe OrdersPersistenceProvider
+    @Test
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    void shouldAddFindAndNotFailWhenNoTransaction() {
+        Order order = OrderTestDataBuilder.anOrder().build();
+        persistenceProvider.add(order);
+
+        Assertions.assertThatNoException().isThrownBy(
+                () -> persistenceProvider.ofId(order.id()).orElseThrow()
+        );
+    }
+}
